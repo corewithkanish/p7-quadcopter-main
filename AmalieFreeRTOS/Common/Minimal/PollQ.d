013c3abaf5850f3bbf1169811f0fedd39622c736
Amalie <apet13@student.aau.dk>
2016-09-30 09:54:17 +0200

    Amalie freertos

--- AmalieFreeRTOS/Common/Minimal/PollQ.d
+++
@@ -1,7 +0,0 @@
-PollQ.o PollQ.d : ../Common/Minimal/PollQ.c ../Source/include/FreeRTOS.h \
-  FreeRTOSConfig.h ../Source/include/projdefs.h \
-  ../Source/include/portable.h ../Source/include/deprecated_definitions.h \
-  ../Source/include/../portable/GCC_ATMega2560/portmacro.h \
-  ../Source/include/mpu_wrappers.h ../Source/include/task.h \
-  ../Source/include/list.h ../Source/include/queue.h \
-  ../Common/include/PollQ.h
--- AmalieFreeRTOS/Source/croutine.d
+++
@@ -1,6 +0,0 @@
-croutine.o croutine.d : ../Source/croutine.c ../Source/include/FreeRTOS.h \
-  FreeRTOSConfig.h ../Source/include/projdefs.h \
-  ../Source/include/portable.h ../Source/include/deprecated_definitions.h \
-  ../Source/include/../portable/GCC_ATMega2560/portmacro.h \
-  ../Source/include/mpu_wrappers.h ../Source/include/task.h \
-  ../Source/include/list.h ../Source/include/croutine.h
