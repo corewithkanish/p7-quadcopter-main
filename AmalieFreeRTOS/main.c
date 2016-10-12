89da3372565a97e0625fbbe16b94dfe6bb79dab2
Niels Skov Vestergaard <Niels Skov Vestergaard>
2016-10-12 14:08:59 +0200

    FreeRTOS that worksgit add .

--- AmalieFreeRTOS/main.c
+++ AmalieFreeRTOS/main.c
@@ -13,29 +13,73 @@
 
 #define LED PORTB
 
+#define configCHECK_FOR_STACK_OVERFLOW 1
+
+#define portUSE_TIMER0
+
+//////////DEBUGGING BLINK///////////////
+// #define LED PORTB
+// while(1)
+// {
+//   LED = 0xFF;
+// }
+////////////////////////////////////////
+
 void vApplicationIdleHook( void );
 
-void LEDBlink(void *pvParameters)
-{
-	while(1)
-	{
-	    	_delay_ms(1000);
-    		LED = 0x00;
-    		_delay_ms(1000);
-    		LED = 0xFF;
-	}
-}
+void LEDBlink(void *pvParameters);
 
 int main()
 {
 	LED_DDR = 0xFF;
+  
+  // #ifdef configUSE_TIMERS
+    // #define configUSE_TIMERS 1
+  // #endif
+  
+	xTaskCreate(LEDBlink, "Blink", 500, NULL, tskIDLE_PRIORITY + 1, NULL);
 
-	xTaskCreate(LEDBlink, "Blink", 500, NULL, 1, NULL);
-
+    // #define LED PORTB
+    // while(1)
+    // {
+      // LED = 0xFF;
+    // }
+    
 	vTaskStartScheduler();
 
 		
   return 0;
+}
+
+void LEDBlink(void *pvParameters)
+{
+  /* The parameters are not used. */
+	( void ) pvParameters;
+  
+	// for( ;; )
+	// {
+	    	// _delay_ms(1000);
+    		// LED = 0x00;
+    		// _delay_ms(1000);
+       while(1)
+       {
+         LED = 0xFF;
+       }
+	// }
+  vTaskDelete( NULL );
+}
+
+
+void vApplicationStackOverflowHook( void ) //Når FreeRTOS ikke har en task den skal lave
+{
+	while(1)
+	{
+		_delay_ms(1000);
+		LED = 0x00;
+		_delay_ms(1000);
+		LED = 0xFF;
+	}
+
 }
 
 void vApplicationIdleHook( void ) //Når FreeRTOS ikke har en task den skal lave
