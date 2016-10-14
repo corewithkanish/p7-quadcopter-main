78df4f9bf671c707dcb9cfa05c6dbe8e6e1385d1
Amalie <apet13@student.aau.dk>
2016-10-14 16:10:14 +0200

    FreeRtos

--- AmalieFreeRTOS/main.c
+++ AmalieFreeRTOS/main.c
@@ -1,3 +1,5 @@
+
+
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -9,88 +11,75 @@
 #include "portmacro.h"
 #include "FreeRTOSConfig.h"
 
-#define LED_DDR DDRB
+#define LED_DDR DDRE
 
-#define LED PORTB
+#define LED PORTE
 
-#define configCHECK_FOR_STACK_OVERFLOW 1
-
-#define portUSE_TIMER0
-
-//////////DEBUGGING BLINK///////////////
-// #define LED PORTB
-// while(1)
-// {
-//   LED = 0xFF;
-// }
-////////////////////////////////////////
 
 void vApplicationIdleHook( void );
 
 void LEDBlink(void *pvParameters);
 
+void LEDnotBlink(void *pvNotParameters);
+
+SemaphoreHandle_t xSemaphore;
+
 int main()
 {
+	
 	LED_DDR = 0xFF;
+	
+	vSemaphoreCreateBinary(xSemaphore);
   
-  // #ifdef configUSE_TIMERS
-    // #define configUSE_TIMERS 1
-  // #endif
-  
-	xTaskCreate(LEDBlink, "Blink", 500, NULL, tskIDLE_PRIORITY + 1, NULL);
+	xTaskCreate(LEDBlink, "Blink", 500, NULL, configMAX_PRIORITIES-1, NULL);
+	xTaskCreate(LEDnotBlink, "notBlink", 500, NULL, configMAX_PRIORITIES-2, NULL);
 
-    // #define LED PORTB
-    // while(1)
-    // {
-      // LED = 0xFF;
-    // }
-    
 	vTaskStartScheduler();
-
-		
+	
   return 0;
 }
 
-void LEDBlink(void *pvParameters)
+void LEDnotBlink(void *pvNotParameters)
 {
-  /* The parameters are not used. */
-	( void ) pvParameters;
-  
-	// for( ;; )
-	// {
-	    	// _delay_ms(1000);
-    		// LED = 0x00;
-    		// _delay_ms(1000);
-       while(1)
-       {
-         LED = 0xFF;
-       }
-	// }
-  vTaskDelete( NULL );
-}
 
-
-void vApplicationStackOverflowHook( void ) //Når FreeRTOS ikke har en task den skal lave
-{
 	while(1)
 	{
-		_delay_ms(1000);
-		LED = 0x00;
-		_delay_ms(1000);
-		LED = 0xFF;
-	}
+		if (xSemaphoreTake(xSemaphore, 100 ))
+			{
+				_delay_ms(1000);
+				LED = 0xFF;
+				_delay_ms(1000);
+				LED = 0xFF;
+				xSemaphoreGive(xSemaphore);
+			}
 
+				vTaskDelay(1000);
+		
+	}
 }
+
+
+void LEDBlink(void *pvParameters)
+{
+
+		while(1)
+		{
+			if (xSemaphoreTake(xSemaphore, 100 ))
+			{
+				_delay_ms(1000);
+				LED = 0x00;
+				_delay_ms(1000);
+				LED = 0x00;
+				xSemaphoreGive(xSemaphore);
+			}
+
+		vTaskDelay(100);
+		}
+}
+
 
 void vApplicationIdleHook( void ) //Når FreeRTOS ikke har en task den skal lave
 {
-		vCoRoutineSchedule();
-	while(1)
-	{
-		_delay_ms(1000);
-		LED = 0x00;
-		_delay_ms(1000);
-		LED = 0xFF;
-	}
+		//vCoRoutineSchedule();
 
 }
