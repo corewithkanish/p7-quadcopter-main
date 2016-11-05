6e529b1e6061fc7b25688262bf4fd7810862c173
Noelia <noelia.va.3003@gmail.com>
2016-11-05 11:26:58 +0100

    Add changes to code

--- AmalieFreeRTOS/functions.h
+++ AmalieFreeRTOS/functions.h
@@ -4,3 +4,8 @@ void AngularController(void);
 void ApplyVelocities(void);
 void PWM_init(int initial_duty);
 void Set_PWM_duty(int duty0, int duty4A, int duty4B, int duty4C);
+int CheckPackageArrival(void);
+void GetPackage(void);
+void USART_Init(unsigned int ubrr);
+unsigned char USART_Receive(void);
+void USART_Transmit(unsigned char data);
